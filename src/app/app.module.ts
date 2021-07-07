0f367cb5e974d3d2f31bc7d37300ac536f867208
Matt <mruesch@amfam.com>
2021-07-07 17:12:28 -0500

    add input form to table component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {  ReactiveFormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
 import { TableComponent } from './components/table/table.component';
@@ -14,6 +16,7 @@ import { DemoMaterialModule } from './modules/material.module';
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule,
     HttpClientModule,
     DemoMaterialModule
--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { DeviceTable } from 'src/app/models/device-table.model';
 import { DeviceService } from 'src/app/services/device.service';
+import { FormGroup, FormControl } from '@angular/forms';
+
 
 @Component({
   selector: 'app-table',
@@ -10,11 +12,20 @@ import { DeviceService } from 'src/app/services/device.service';
 export class TableComponent implements OnInit {
 
   deviceTable: DeviceTable;
+  form: FormGroup;
 
   constructor(private deviceService: DeviceService) { }
 
   ngOnInit(): void {
     this.setupTable();
+
+    this.form = new FormGroup({
+      searchKey: new FormControl(''),
+    });
+
+    this.form.controls['searchKey'].valueChanges.subscribe(value => {
+      console.log(value);
+    });
   }
 
   private setupTable(): void {
